493922e8aa5e62c0b0e81722d97e466c833c852e
arnobfintech07 <arnob@fintech-point.com>
2025-12-17 13:35:07 +0600

    commented all the codes properly

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// A fuction has been created named as romanNumerals and num as parameter
 
 function romanNumerals(num) {
   const values = [1000, 500, 100, 50, 40, 10, 5, 4, 1];
   const symbols = ["M", "D", "C", "L", "XL", "X", "V", "IV", "I"];
+  
+  //at first result will be empty and all the symbols will store after the completion of a loop
+  
   let result = "";
+  
+  //this while loop will run until the value of num is 0
   while (num) {
 
     //added the logic to satisfy all the test cases
@@ -25,8 +31,10 @@ function romanNumerals(num) {
 
       if (parseInt(num / values[i]) > 0) {
         for (let k = 0; k < parseInt(num / values[i]); k++) {
+          //result will store the symbols one by one
           result += symbols[i];
         }
+        //the remainder will store in num and the num will be updated
         num = num % values[i];
         break;
       }
